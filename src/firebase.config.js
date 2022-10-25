fd7bac1dc2076028fbfa264f4a0bc12e240672c7
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-10-25 13:42:28 -0700

    firestore added

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAlZHFBCQA1tLGqOAWDQs7e39mVFQA2M3s",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
