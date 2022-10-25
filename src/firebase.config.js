3c6b173cb9be5fb6e31cc5a55b317305fa85074b
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-10-25 13:35:06 -0700

    install firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlZHFBCQA1tLGqOAWDQs7e39mVFQA2M3s",
+    authDomain: "house-marketplace-cf14c.firebaseapp.com",
+    projectId: "house-marketplace-cf14c",
+    storageBucket: "house-marketplace-cf14c.appspot.com",
+    messagingSenderId: "762792333597",
+    appId: "1:762792333597:web:7ad4dd0204c427a98ed1f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
